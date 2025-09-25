cd1e099268ccbbf572142e19b572bed1073c4369
ZHir0bubeL <EgAnastasia2006@yandex.ru>
2025-09-25 13:28:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,39 @@ hostname = socket.gethostname()
 
 while True:
     command = input(f"\n{username}@{hostname}:~$ ")
+    key = False
     for name in os.environ:
         command = command.replace("$" + name, os.environ[name])
     command = command.split()
 
-    if command[0] == "cd":
-        print(command[0] + ':', end=' ')
-        for i in range(1, len(command)):
-            if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$":
-                print(command[i], end=' ')
-            else:
-                continue
+    for j in range(len(command)):
+        if command[j] == "cd":
+            print(command[j] + ':', end=' ')
+            key = True
+            for i in range(j + 1, len(command)):
+                if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
+                    print(command[i], end=' ')
+                else:
+                    j = i
+                    print("\n", end='')
+                    break
 
-    elif command[0] == "ls":
-        print(command[0] + ':', end=' ')
-        for i in range(1, len(command)):
-            if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$":
-                print(command[i], end=' ')
-            else:
-                continue
+        elif command[j] == "ls":
+            key = True
+            print(command[j] + ':', end=' ')
+            for i in range(j + 1, len(command)):
+                if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
+                    print(command[i], end=' ')
+                else:
+                    j = i
+                    print("\n", end='')
+                    break
 
-    elif command[0] == "exit":
-        print("Exit")
-        break
+        elif command[j] == "exit":
+            print("Exit")
+            break
 
-    else:
-        print("Invalid command")
+        elif not key:
+            print("Invalid command")
+
+    break
