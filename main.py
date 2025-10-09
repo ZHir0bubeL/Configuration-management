4fe68fd412b4fdf915db4a127f80c2ff87118f5b
ZHir0bubeL <EgAnastasia2006@yandex.ru>
2025-10-09 13:24:33 +0300

    file

--- main.py
+++ main.py
@@ -3,52 +3,51 @@ import socket
 
 username = os.getlogin()
 hostname = socket.gethostname()
+key_exit = False
 
-while True:
-    command = input(f"\n{username}@{hostname}:~$ ")
-    key = False
-    key_exit = False
-    for name in os.environ:
-        command = command.replace("$" + name, os.environ[name])
-    command = command.split()
+def emulation(command):
+        key = False
+        global key_exit
+        for name in os.environ:
+            command = command.replace("$" + name, os.environ[name])
+        command = command.split()
 
-    for j in range(len(command)):
-        if command[j] == "cd":
-            print(command[j] + ':', end=' ')
-            key = True
-            for i in range(j + 1, len(command)):
-                if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
-                    print(command[i], end=' ')
-                else:
-                    j = i
-                    print("\n", end='')
-                    break
+        for j in range(len(command)):
+            if command[j] == "cd":
+                print(command[j] + ':', end=' ')
+                key = True
+                for i in range(j + 1, len(command)):
+                    if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
+                        print(command[i], end=' ')
+                    else:
+                        j = i
+                        print("\n", end='')
+                        break
 
-        elif command[j] == "ls":
-            key = True
-            print(command[j] + ':', end=' ')
-            for i in range(j + 1, len(command)):
-                if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
-                    print(command[i], end=' ')
-                else:
-                    j = i
-                    print("\n", end='')
-                    break
+            elif command[j] == "ls":
+                key = True
+                print(command[j] + ':', end=' ')
+                for i in range(j + 1, len(command)):
+                    if command[i] != "ls" and command[i] != "cd" and command[i][0] != "$" and command[i] != "exit":
+                       print(command[i], end=' ')
+                    else:
+                        j = i
+                        print("\n", end='')
+                        break
 
-        elif command[j] == "exit":
-            print("Exit")
-            key_exit = True
-            break
+            elif command[j] == "exit":
+                print("Exit")
+                key_exit = True
+                break
 
-        elif not key:
-            print("Invalid command")
-            break
+            elif not key:
+                print("Invalid command")
+                break
 
-    if key_exit: break
-
-with open("/vfs.txt", "r") as f:
+with open("vfs.txt", "r") as f:
     for line in f.readlines():
         if line.startswith("#"):
             continue
-        print("vfs@", line.rstrip())
-        print(emulation(line))
+        if not key_exit:
+            print("vfs@", line.rstrip())
+            print(emulation(line))
